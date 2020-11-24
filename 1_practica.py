f2f424679bfa8fd71a8d80def5452715d0a7f44c
Tomas Ignacio Garcia <15tommgarcia@gmail.com>
2020-11-24 11:50:33 -0300

    Practico while, for, string methods

--- 1_practica.py
+++ 1_practica.py
@@ -1,3 +1,5 @@
+'''
+
 a = 10
 cad = 'hola'
 
@@ -9,3 +11,20 @@ print(cad, nombre)
 num = int(input('Ingrese un numero entero: '))
 
 print('El numero ingrasado es %d'%(num))
+
+if num==0:
+    print('El numero ingrasado %d, es CERO'%(num))
+elif num%2==0:
+    print('El numero ingrasado %d, es PAR'%(num))
+else:
+    print('El numero ingrasado %d, es IMPAR'%(num))
+
+'''
+
+a='programacion'
+print(a[:4]) #output= prog
+print(a[6:]) #output= macion
+print(len(a)) #output= 12
+print(a.upper()) # =PROGRAMACION
+print(a.lower()) # =programacion
+print(a.capitalize()) # =Programacion
